7066e976c23e1301c3d32f111053443bb6139d58
Defstrong <sfayzulloev1@gmail.com>
2023-02-21 16:45:11 +0500

    Add project files.

---
+++ Algorithms/Algorithms/BinarySearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Algorithms.Algorithms
+{
+    static public class BinarySearchAlgorithm
+    {
+        static public (int idxFindVal, bool haveOrNot) BinarySearch(this int[] array, int findVal)
+        {
+            int low = 0;
+            int high = array.Length - 1, middle = (low + high) / 2;
+            while ((array[middle] != findVal) && low <= high)
+            {
+                middle = (low + high) / 2;
+
+                if (array[middle] < findVal)
+                    low = middle + 1;
+                else if (array[middle] > findVal)
+                    high = middle - 1;
+            }
+            if (array[middle] == findVal)
+                return (middle, true);
+            return (middle, false);
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using Algorithms.Algorithms;
+
+int findNumber = int.Parse(Console.ReadLine());
+var array = new int[] { 1, 5, 7, 12, 14, 15, 17, 18, 19, 110 };
+
+var final = array.BinarySearch(findNumber);
+
+if (final.haveOrNot)
+    Console.WriteLine(final.idxFindVal + 1);
+else
+    Console.WriteLine("Error 404. Not found!!!");
