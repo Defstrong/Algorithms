a2b61be8e1a193c36e00e23f6f6defd764de4d37
Defstrong <sfayzulloev1@gmail.com>
2023-02-21 17:20:51 +0500

    AddRecursiveBinarySearch

---
+++ Algorithms/Algorithms/RecursiveBinarySearchAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Algorithms.Algorithms
+{
+    static public class RecursiveBinarySearchAlgorithm
+    {
+        static public (int idxFindVal, bool hereOrNo) RecursiveBinarySearch(this int[] array, int lowReturn, int highReturn, int findVal)
+        {
+            int low = lowReturn, high = highReturn;
+            int middle = low + (high - low) / 2;
+            if (low >= high)
+                return (-1, false);
+            if (array[middle] == findVal)
+                return (middle, true);
+
+
+            if (array[middle] < findVal)
+                return array.RecursiveBinarySearch(middle + 1, high, findVal);
+            else
+                return array.RecursiveBinarySearch(low, middle, findVal);
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -4,9 +4,4 @@ using Algorithms.Algorithms;
 int findNumber = int.Parse(Console.ReadLine());
 var array = new int[] { 1, 5, 7, 12, 14, 15, 17, 18, 19, 110 };
 
-var final = array.BinarySearch(findNumber);
-
-if (final.haveOrNot)
-    Console.WriteLine(final.idxFindVal + 1);
-else
-    Console.WriteLine("Error 404. Not found!!!");
+Console.WriteLine(array.RecursiveBinarySearch(0, array.Length, findNumber).idxFindVal+1);
